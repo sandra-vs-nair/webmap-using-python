e85029669af4f46302445a93f9c898a19cc1d834
Sandra VS Nair <sandravsnair@gmail.com>
2020-05-28 10:00:40 +0530

    Update map.py

--- map.py
+++ map.py
@@ -24,7 +24,7 @@ for lat,lon,city in zip(latitude,longitude,cities):
     group1.add_child(folium.Marker(location=[lat, lon],popup="Welcome to "+city+" !",icon=folium.Icon(color='red')))
 
 #Adding polygons as country boundaries and color code according to population.
-group2.add_child(folium.GeoJson(data=open("data/polygons.json",'r',encoding='utf-8-sig').read(),style_function=lambda x :{'fillColor':'yellow' if x['properties']['POP2005']<10000000 else 'green' if 10000000 <x['properties']['POP2005']>20000000 else 'red'}))
+group2.add_child(folium.GeoJson(data=open("data/polygons.json",'r',encoding='utf-8-sig').read(),style_function=lambda x :{'fillColor':'yellow' if x['properties']['POP2005']<10000000 else 'green' if 10000000 <=x['properties']['POP2005']<=20000000 else 'red'}))
 
 map.add_child(group1)
 map.add_child(group2)
